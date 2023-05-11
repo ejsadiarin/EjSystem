19cd39bf848b01463d66cb00da29be24cfc927a2
ejsadiarin <ejsadiarin@gmail.com>
2023-05-11 14:16:25 +0800

    getting started - initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -38,6 +38,9 @@ export default function Home() {
           priority
         />
       </div>
+      <div>
+        Ej Sadiarin - Ej System
+      </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
